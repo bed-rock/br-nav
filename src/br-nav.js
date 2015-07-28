1e91fb2e3257ca6763e445d0de76a37ae5d19501
marcelo <marceloribeiromidia@gmail.com>
2015-07-28 18:41:49 -0300

    Demo and design improvements

--- src/br-nav.js
+++ src/br-nav.js
@@ -14,8 +14,8 @@ var tid = setInterval( function () {
 
   var querySelector = document.querySelector.bind(document);
 
-  var nav = document.querySelector('.page-nav');
-  var wrapper = document.querySelector('.wrapper');
+  var nav = document.querySelector('br-nav');
+  // var wrapper = document.querySelector('.wrapper');
 
   var menu = document.getElementById("js-menu");
   var subnavs = menu.querySelectorAll('.menu--item__has_submenu');
@@ -45,25 +45,10 @@ var tid = setInterval( function () {
   }
 
 
-  // Minify menu on menu_minifier click
-  querySelector('.collapse_menu').onclick = function () {
-
-    nav.classList.toggle('page-nav__minify');
-
-    wrapper.classList.toggle('wrapper__minify');
-
-    for (var j = 0; j < subnavs.length; j++) {
-
-      subnavs[j].classList.remove('submenu__opened');
-
-    }
-  };
-
-
   // Toggle menu click
   querySelector('.toggle_menu').onclick = function () {
     nav.classList.toggle('page-nav__opened');
-    wrapper.classList.toggle('toggle-content');
+    // wrapper.classList.toggle('toggle-content');
   };
 
 
