a2d38104d72d235f0d3d548aff76d6991b3e998a
marcelo <marceloribeiromidia@gmail.com>
2015-07-29 19:45:49 -0300

    Visual improvements.

--- src/br-nav.js
+++ src/br-nav.js
@@ -14,30 +14,29 @@ var tid = setInterval( function () {
 
   var querySelector = document.querySelector.bind(document);
 
-  var nav = document.querySelector('br-nav');
+  var nav = querySelector('br-nav');
   // var wrapper = document.querySelector('.wrapper');
 
-  var menu = document.getElementById("js-menu");
-  var subnavs = menu.querySelectorAll('.menu--item__has_submenu');
+  var menu = querySelector(".js-menu");
+  var item = menu.querySelectorAll('.menu--item__has_submenu');
 
 
   // Open Sub Menu
-  for (var i = 0; i < subnavs.length; i++) {
+  for (var i = 0; i < item.length; i++) {
 
-    if (subnavs[i].classList.contains('menu--item__has_submenu') ) {
+    if (item[i].classList.contains('menu--item__has_submenu') ) {
 
-      subnavs[i].addEventListener('click', function (e) {
-
+      item[i].addEventListener('click', function (e) {
         e.preventDefault();
 
-        for (var j = 0; j < subnavs.length; j++) {
+        for (var j = 0; j < item.length; j++) {
 
-          if(this != subnavs[j])
-            subnavs[j].classList.remove('submenu__opened');
+          if(this != item[j])
+            item[j].classList.remove('menu--item__has_submenu__opened');
 
         }
 
-        this.classList.toggle('submenu__opened');
+        this.classList.toggle('menu--item__has_submenu__opened');
 
       }, false);
 
@@ -48,7 +47,6 @@ var tid = setInterval( function () {
   // Toggle menu click
   querySelector('.toggle_menu').onclick = function () {
     nav.classList.toggle('page-nav__opened');
-    // wrapper.classList.toggle('toggle-content');
   };
 
 
